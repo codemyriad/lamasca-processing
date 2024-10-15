f62fb227cfd6b5db983686f87be30a40231a662c
Silvio Tomatis <silviot@gmail.com>
2024-10-15 22:57:49 +0200

    feat: Add environment variable support for model path

--- src/lp_labelstudio/constants.py
+++ src/lp_labelstudio/constants.py
@@ -1,10 +1,11 @@
+import os
 from pathlib import Path
 
 # File extensions
 JPEG_EXTENSION = ".jpeg"
 
 # Model paths
-NEWSPAPER_MODEL_PATH = "lp://NewspaperNavigator/faster_rcnn_R_50_FPN_3x/config"
+NEWSPAPER_MODEL_PATH = os.environ.get("MODEL_PATH", "lp://NewspaperNavigator/faster_rcnn_R_50_FPN_3x/config")
 NEWSPAPER_LABEL_MAP = {
     0: "Photograph",
     1: "Illustration",
