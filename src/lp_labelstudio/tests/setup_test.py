945e3183b4c26dc399333f12882422b19f2e4c05
Silvio Tomatis <silviot@gmail.com>
2024-11-25 16:12:43 +0100

    test: Add setup_test.py for label studio tests

---
+++ src/lp_labelstudio/tests/setup_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from pathlib import Path
+import requests
+
+
+TEST_FILES_FROOT = Path('/tmp/lamasca-tests')
+BASEURL = 'https://newspapers.codemyriad.io/lamasca-pages/'
+
+PAGES_NEEDED = [
+    '1994/lamasca-1994-02-16/page_01.jpeg',
+    '1994/lamasca-1994-02-16/page_02.jpeg',
+    '1994/lamasca-1994-02-16/page_02.jpeg',
+    '1994/lamasca-1994-02-16/manifest.json'
+]
+
+def prepare():
+    # Download the pages needed for the test
+    # from https://newspapers.codemyriad.io/lamasca-pages/
+    # to /tmp/lamasca-tests
+    for filepath in PAGES_NEEDED:
+        fullpath = TEST_FILES_FROOT / filepath
+        if not fullpath.exists():
+            print(f"Downloading {fullpath}")
+            url = BASEURL + filepath
+            response = requests.get(url)
+            # Save the file in `fullpath`
+
+if __name__ == '__main__':
+    prepare()
