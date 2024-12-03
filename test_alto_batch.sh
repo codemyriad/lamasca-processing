057bf1c05292badca1fa4f53532c8bb9aef8ab3a
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2024-12-03 15:54:35 +0100

    Add images and fix startup error

--- test_alto_batch.sh
+++ test_alto_batch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-set -x
 
 # Check if input file is provided
 if [ $# -eq 0 ]; then
@@ -25,6 +24,8 @@ mkdir -p "${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}"
 
 # Copy Alto XML to batch directory
 cp "$ALTO_XML_FILE" "${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}/0001.xml"
+cp "$ALTO_XML_FILENAME.jp2" "${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}/0001.jp2"
+cp "$ALTO_XML_FILENAME.tif" "${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}/0001.tif"
 
 # Create a dummy batch_xml file (required for Open ONI batch)
 cat << EOF > "${BATCH_DIR}/${BATCH_NAME}/data/batch.xml"
@@ -36,10 +37,6 @@ EOF
 
 # Copy default mets file
 cp test_batches/default_mets.xml ${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}/${BATCH_DATE}.xml
-
-# Create a dummy image to satisfy Open ONI requirements
-convert -size 2000x3000 xc:white "${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}/0001.jp2"
-convert -size 2000x3000 xc:white "${BATCH_DIR}/${BATCH_NAME}/data/sn00000001/001/${BATCH_DATE}/0001.tif"
 
 # Path to Open ONI docker-compose file (adjust as needed)
 DOCKER_COMPOSE_PATH="./docker-compose.yml"
@@ -59,9 +56,6 @@ docker compose -f "$DOCKER_COMPOSE_PATH" up -d
 # Wait for the container to fully start
 sleep 15
 
-# Change permissions for the django cache
-docker compose -f "$DOCKER_COMPOSE_PATH" exec -T web bash -c "chown -R www-data:www-data /var/tmp/django_cache"
-
 # Create Awardee and Title objects if they don't exists
 docker compose -f "$DOCKER_COMPOSE_PATH" exec -T web bash -c "source ENV/bin/activate && python manage.py shell < /opt/create_awardee_title.py"
 
@@ -70,6 +64,11 @@ docker compose -f "$DOCKER_COMPOSE_PATH" exec -T web bash -c "source ENV/bin/act
 
 # Load the batch into Open ONI
 docker compose -f "$DOCKER_COMPOSE_PATH" exec -T web bash -c "source ENV/bin/activate && python manage.py load_batch /opt/openoni/data/batches/$BATCH_NAME"
+
+docker compose -f "$DOCKER_COMPOSE_PATH" exec -T web bash -c "curl -s http://localhost/lccn/sn00000001/1994-01-12/ed-1/seq-1/"
+
+# Change permissions for the django cache
+docker compose -f "$DOCKER_COMPOSE_PATH" exec -T web bash -c "chown -R www-data:www-data /var/tmp/django_cache"
 
 echo "Batch ${BATCH_NAME} loaded successfully!"
 
